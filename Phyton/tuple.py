db8fa98c9fa88d53ad60b91e306c7f585d65428d
Atulsharma0001 <atulsharma99018@gmail.com>
2026-07-14 21:58:25 +0530

    Add files via upload

---
+++ Phyton/tuple.py
@@ -0,0 +1,8 @@
+a=(1,2,'man','dog')
+
+print(type(a))              # it show it is tuple
+ #a[2]= ddd                show error bcz tuple cannot be changed similar to string
+print(a)
+print(a[0])
+
+
